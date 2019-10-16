5179b3f0846a08b0b667c71ecdec50ee2e679b81
elina <badianeelina8@gmail.com>
2019-10-16 17:25:55 +0200

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  loginUserData = {};
+  helper = new JwtHelperService();
+
+
+  constructor(private auth: AuthService,
+              private router: Router) {}
+
+              ngOnInit() {
+              }
+
+  loginUser() {
+    this.auth.loginUser(this.loginUserData)
+    .subscribe(
+      res => {
+        console.log(this.loginUserData);
+        console.log(res);
+
+        if ( res.token) {
+       localStorage.setItem('token', res.token);
+       const decodedToken = this.helper.decodeToken(res.token);
+       console.log(decodedToken.username);
+       localStorage.setItem('username', decodedToken.username);
+       localStorage.setItem('roles', decodedToken.roles);
+       localStorage.setItem('expiration', decodedToken.exp);
+       console.log(localStorage);
+       this.rol(decodedToken.roles[0]);
+       this.router.navigate(['/home']);
+}
+   },
+   err => {
+    console.log(err);
+    }
+     );
+   }
+   rol(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+
+}
+
